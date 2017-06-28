fc9f9c42335d0e6a7598e9495ba7f224a0803efa
Sean coyne <scoyne2@kent.edu>
2017-06-28 07:59:40 -0500

    Initial commit

---
+++ Scripts/CoreMeasures.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+    var isConnected = $('#isConnected').val();
+
+    if (isConnected == "True") {
+        $('#parameter2').prop("disabled", false);
+        $('#parameter3').prop("disabled", false);
+        $('#parameter4').prop("disabled", false);
+        $('#submitButton').prop("disabled", false);     
+    };
+
+    if (isConnected == "False") {
+        $('#parameter2').prop("disabled", true);
+        $('#parameter3').prop("disabled", true);
+        $('#parameter4').prop("disabled", true);
+        $('#submitButton').prop("disabled", true);
+    };    
+
+});
